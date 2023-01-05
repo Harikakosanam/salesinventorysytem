84b27940869ef0f08c77edd08141b0416eed8979
Harikakosanam <harika.kosanam@gmail.com>
2023-01-05 11:42:52 +0530

    main branch has been created

---
+++ src/test/java/salesinventory/Inventory.java
@@ -0,0 +1,10 @@
+package salesinventory;
+
+public class Inventory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/salesinventory/Sales.java
@@ -0,0 +1,9 @@
+package salesinventory;
+
+public class Sales {
+
+	public static void main(String[] args) {
+System.out.println("hello");
+	}
+
+}
